08f9e61095257b4f1c5a389b74397401b3d978e9
Jonadan12 <nadan775712@gmail.com>
2023-10-04 22:33:05 +0900

    #3 미션 1차 4번째 체크리스트

--- udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MusicFileFragment.kt
+++ udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MusicFileFragment.kt
@@ -6,14 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentLockerBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
-class LockerFragment : Fragment() {
-
+class MusicFileFragment:Fragment() {
     lateinit var binding: FragmentLockerBinding
-
-
-        private val information= arrayListOf("저장한 곡","음악 파일")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +16,6 @@ class LockerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
-
-        val lockerAdapter = LockerVPAdapter(this)
-        binding.fragmentContentVp.adapter = lockerAdapter
-        TabLayoutMediator(binding.fragmentLockerRestoreContentTb, binding.fragmentContentVp){
-                tab, position ->
-            tab.text = information[position]
-        }.attach()
-
 
         return binding.root
     }
